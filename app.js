3dc31630d4eb873a0af5202ffcd4ce90bd03d71f
Vartan Arabyan <vartana@gmail.com>
2015-01-01 22:58:22 -0700

    All on

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var schedule = require('node-schedule');
 var Trigger = require('./lib/relay');
 
-var waterpumps = [1, 2, 3, 4, 5];
+var allRelays = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
+
+Trigger(allRelays, true, comp);
+
+function comp(){
+
+  console.log("complete");
+}
