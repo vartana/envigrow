194116177caedb29b1d2b9d8f78d96e066987581
Vartan Arabyan <vartana@gmail.com>
2015-01-01 23:20:03 -0700

    False for relay

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var Trigger = require('./lib/relay');
 
 var allRelays = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
 
-Trigger(allRelays, true, comp);
+Trigger(allRelays, false, comp);
 
 function comp(){
   console.log("complete");
