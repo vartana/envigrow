ffdca5fe995375d09c66695472e82d642fffb651
Vartan Arabyan <vartana@gmail.com>
2015-01-05 15:54:21 -0700

    Created Schedules

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var gpio = require('rpi-gpio');
 
 var Trigger = require('./lib/relay').Trigger;
 var Relay = require('./lib/relay').Relay;
-var timer = require('./scheudles');
+var timer = require('./schedules');
 
 //Setup FALSE = on / TRUE = off
 var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
