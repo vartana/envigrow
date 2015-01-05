ce968afb5146d186384383424a9b72f47de52f56
Vartan Arabyan <vartana@gmail.com>
2015-01-05 11:51:16 -0700

    mods

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var schedule = require('node-schedule');
 var Trigger = require('./lib/relay');
 
-var allRelays = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
+var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
+var recirculation = [3, 5, 7, 8, 10, 11, 12, 13];
+var feeding = [15, 16, 18, 19, 21, 22, 23, 24];
 
-Trigger(allRelays, true, comp);
+Trigger(allPins, true, comp);
+Trigger(recirculation, false, comp);
+Trigger(feeding, false, comp);
 
 function comp(){
   console.log("complete");
