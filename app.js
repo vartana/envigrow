c57fe94d5b1cdf9803be2efd271fb25d617e1dc5
Vartan Arabyan <vartana@gmail.com>
2015-01-05 11:54:00 -0700

    mods

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 2
 var recirculation = [3, 5, 7, 8, 10, 11, 12, 13];
 var feeding = [15, 16, 18, 19, 21, 22, 23, 24];
 
-Trigger(allPins, true, comp);
-Trigger(recirculation, false, comp);
-Trigger(feeding, false, comp);
+Trigger(allPins, false, comp);
+Trigger(recirculation, true, comp);
+Trigger(feeding, true, comp);
 
 function comp(){
   console.log("complete");
