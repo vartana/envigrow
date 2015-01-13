eaac905b44ee690af0e5db19f8b463a391214059
Vartan Arabyan <vartana@gmail.com>
2015-01-13 01:30:59 -0700

    Added server logging features

--- app.js
+++ app.js
@@ -4,29 +4,39 @@ var gpio = require('rpi-gpio');
 var Trigger = require('./lib/relay').Trigger;
 var Relay = require('./lib/relay').Relay;
 var timer = require('./schedules');
+var EnviLog = require('./lib/envilog');
 
 //Setup FALSE = on / TRUE = off
 var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
 
+EnviLog({ status: 'info', message: 'Server started'}, Start);
 
-Trigger(allPins, true, function(err){
-  setTimeout(function(){ 
-    if(err){
-          console.log('Error', err);
-    }
+function Start(){
 
-    timer.forEach(function (el, index, array) {
-      // var rule = new schedule.RecurrenceRule();
-      // rule.minute = 90;
+  Trigger(allPins, true, function(err){
+    setTimeout(function(){ 
+      if(err){
+            console.log('Error', err);
+      }
 
-      schedule.scheduleJob('*/90 * * * *', Feed.bind(null, el));
-      Relay(el.circ, false);
-      Relay(el.feed, true);
-    });
-  }, 10 * 1000);
-});
+      EnviLog({ status: 'info', message: 'Initiated GPIO ports and 90min timer started'});
+
+      timer.forEach(function (el, index, array) {
+
+        schedule.scheduleJob('*/90 * * * *', Feed.bind(null, el));
+        Relay(el.circ, false);
+        Relay(el.feed, true);
+      });
+
+
+    }, 10 * 1000);
+  }); 
+}
+
 
 function Feed(el){
+
+  EnviLog({ status: 'info', message: 'Feed started started'});
 
   Relay(el.circ, true);
   Relay(el.feed, false);
@@ -37,6 +47,7 @@ function Feed(el){
     Relay(el.circ, false);
     Relay(el.feed, true);
 
+    EnviLog({ status: 'info', message: 'Feed completed'});
     console.log('Feed Stop ', el.circ);
   }, el.sec * 1000)
 }
