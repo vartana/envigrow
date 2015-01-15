824bdf9fb5fbf4e5430f67b12158ceaafc2024a7
Vartan Arabyan <vartana@gmail.com>
2015-01-15 12:29:07 -0700

    wiring pi

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 2
 console.log('Initiate GPIO');
 allPins.forEach(function (el, index, array) {
 
-  gpio.open(el);
+  gpio.open(el, 'output');
 });
 
 console.log('Timer Start');
