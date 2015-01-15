1109c8f4370e097f6360f8800e7b8dccc70e7b1e
Vartan Arabyan <vartana@gmail.com>
2015-01-15 12:49:46 -0700

    wiring pi

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var Relay = require('./lib/relay').Relay;
 var timer = require('./schedules');
 var EnviLog = require('./lib/envilog');
 
-//Setup FALSE = on / TRUE = off
+//Setup 0 = on / 1 = off
 var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
 
 //EnviLog({ status: 'info', message: 'Server started'});
@@ -25,15 +25,14 @@ console.log('Timer Start');
 function Feed(el){
 
   EnviLog({ status: 'info', message: 'Feed started started: '+ el.feed});
+  console.log('Feed Start ', el.circ);
 
   Relay(el.circ, 1);
   Relay(el.feed, 0);
 
-  console.log('Feed Start ', el.circ);
-
   setTimeout(function(){
-    Relay(el.circ, 1);
-    Relay(el.feed, 0);
+    Relay(el.circ, 0);
+    Relay(el.feed, 1);
 
     EnviLog({ status: 'info', message: 'Feed completed: '+el.feed});
     console.log('Feed Stop ', el.circ);
