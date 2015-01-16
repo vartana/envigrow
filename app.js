463fad8ec14f92b7c46a4fc28512bf2afea6de4d
Vartan Arabyan <vartana@gmail.com>
2015-01-15 18:19:03 -0700

    wiring pi

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 2
 
 //EnviLog({ status: 'info', message: 'Server started'});
 //EnviLog({ status: 'info', message: 'Initiated GPIO ports and 90min timer started'});
+
+console.log('All Feed Off');
+allPins.forEach(function (el, index, array) {
+    
+    Feed(el, false);
+});
+
 console.log('Timer Start');
 
 schedule.scheduleJob('*/2 * * * *', function(){
@@ -18,8 +25,6 @@ schedule.scheduleJob('*/2 * * * *', function(){
   console.log('Timer Executed');
   Start();
 });
-
-
 
 function Start(){
 
