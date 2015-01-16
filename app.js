92c08a64c3550115271603fb70e920d1141d2bbb
Vartan Arabyan <vartana@gmail.com>
2015-01-15 18:20:50 -0700

    wiring pi

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 2
 console.log('All Feed Off');
 allPins.forEach(function (el, index, array) {
     
-    console.log(el);
-    Feed(el, false);
+    Relay(el, 1);
 });
 
 console.log('Timer Start');
