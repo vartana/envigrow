55d4149a90fa6318965359bbcebf131afee79e56
Vartan Arabyan <vartana@gmail.com>
2015-01-15 18:49:01 -0700

    wiring pi

--- schedules.js
+++ schedules.js
@@ -4,11 +4,11 @@
 // 12 = system 7
 // 11 = systems 8-9
 var systems = [
-  {circ: 5, feed: 21, sec: 50},
-  {circ: 13, feed: 18, sec: 50},
-  {circ: 3, feed: 24, sec: 60},
-  {circ: 12, feed: 22, sec: 50},
-  {circ: 11, feed: 19, sec: 55}
+  {circ: 5, feed: 21, sec: 40},
+  {circ: 13, feed: 18, sec: 40},
+  {circ: 3, feed: 24, sec: 50},
+  {circ: 12, feed: 22, sec: 40},
+  {circ: 11, feed: 19, sec: 45}
 ];
 
 module.exports = systems;
