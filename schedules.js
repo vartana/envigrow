cf1372b3025518186ea74de0a0c5879216f60dc3
Vartan Arabyan <vartana@gmail.com>
2015-01-30 15:28:56 -0700

    submit

--- schedules.js
+++ schedules.js
@@ -11,7 +11,7 @@ var systems = [
   {circ: 13, feed: 18, sec: 35},
   {circ: 3, feed: 24, sec: 35},
   {circ: 12, feed: 22, sec: 35},
-  {circ: 11, feed: 19, sec: 35}
+  {circ: 11, feed: 19, sec: 35},
   {circ: 7, feed: 23, sec: 35},
   {circ: 8, feed: 15, sec: 35},
   {circ: 10, feed: 16, sec: 35}
