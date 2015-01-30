0cf1e8e4cbdb18c25fd17a182952bd51220d6dfc
vhgrower <vhgrower@gmail.com>
2015-01-30 12:35:41 -0700

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -4,11 +4,15 @@
 // 12 = system 7
 // 11 = systems 8-9
 var systems = [
-  {circ: 5, feed: 21, sec: 40},
-  {circ: 13, feed: 18, sec: 40},
-  {circ: 3, feed: 24, sec: 40},
-  {circ: 12, feed: 22, sec: 40},
-  {circ: 11, feed: 19, sec: 40}
+  {circ: 5, feed: 21, sec: 35},
+  {circ: 13, feed: 18, sec: 35},
+  {circ: 3, feed: 24, sec: 35},
+  {circ: 12, feed: 22, sec: 35},
+  {circ: 11, feed: 19, sec: 35}
+  {circ: 15, feed: 7, sec: 35},
+  {circ: 16, feed: 8, sec: 35},
+  {circ: 22, feed: 10, sec: 35}
+  
 ];
 
 module.exports = systems;
