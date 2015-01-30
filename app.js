75ebe5eb6d374f41de4499693e4876f75607c67b
Vartan Arabyan <vartana@gmail.com>
2015-01-30 16:46:39 -0700

    submit

--- app.js
+++ app.js
@@ -1,52 +1,46 @@
 var schedule = require('node-schedule');
 var gpio = require('pi-gpio');
 
-var Trigger = require('./lib/relay').Trigger;
 var Relay = require('./lib/relay').Relay;
+var gpioInit = require('./lib/relay').Init;
 var timer = require('./schedules');
 var EnviLog = require('./lib/envilog');
 
 //Setup 0 = on / 1 = off
-var allPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40];
-
 EnviLog({ status: 'init', message: 'Server started'});
-
-
 EnviLog({ status: 'init', message: 'Turning all relays off'});
-allPins.forEach(function (el, index, array) {
-    Relay(el, 1);
+gpioInit();
+
+
+
+
+setTimeout(function(){
+  EnviLog({ status: 'init', message: 'Turning all recirculation systems on'});
+  timer.forEach(function (el, index, array) {
+
+    Relay(el.circ, 0);
 
     if (index === array.length - 1) {
+      // var rule = new schedule.RecurrenceRule();
+      // rule.minute = 90;
 
-      setTimeout(function(){
-        EnviLog({ status: 'init', message: 'Turning all recirculation systems on'});
-        timer.forEach(function (el, index, array) {
-      
-          Relay(el.circ, 0);
+      // schedule.scheduleJob(rule,function(){
+      //   EnviLog({ status: 'test', message: 'testing minute rule for 90 minutes'});
+      // });
 
-          if (index === array.length - 1) {
-            // var rule = new schedule.RecurrenceRule();
-            // rule.minute = 90;
+      schedule.scheduleJob('0 0,3,6,9,12,15,18,21 * * *', function(){
+        EnviLog({ status: 'init', message: 'Cron job for system cycle every 90 minutes'});
+        Start();
+      });
 
-            // schedule.scheduleJob(rule,function(){
-            //   EnviLog({ status: 'test', message: 'testing minute rule for 90 minutes'});
-            // });
-
-            schedule.scheduleJob('0 0,3,6,9,12,15,18,21 * * *', function(){
-              EnviLog({ status: 'init', message: 'Cron job for system cycle every 90 minutes'});
-              Start();
-            });
-
-            schedule.scheduleJob('30 1,4,7,10,13,16,19,22 * * *', function(){
-              EnviLog({ status: 'init', message: 'Cron job for system cycle every 90 minutes'});
-              Start();
-            });
-          }
-        }, 35000);
-      })
+      schedule.scheduleJob('30 1,4,7,10,13,16,19,22 * * *', function(){
+        EnviLog({ status: 'init', message: 'Cron job for system cycle every 90 minutes'});
+        Start();
+      });
     }
-    
+  }, 60000);
 });
+
 
 function Start(){
 
