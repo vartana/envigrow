3d0a8a353ba9d34522050611f539d6b110700872
Vartan Arabyan <vartana@gmail.com>
2015-02-24 14:32:04 -0700

    fixes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ gpioInit();
 console.log(timer.A);
 
 var sysA = require('./lib/procsys');
-sysA.Init(timer.A, 6);
+sysA().Init(timer.A, 6);
 
 var sysB = require('./lib/procsys');
-sysB.Init(timer.B, 3);
+sysB().Init(timer.B, 3);
