d7b8722c696d08e2bc2ed001c93e2b8e382ad16b
vhgrower <vhgrower@gmail.com>
2016-05-16 16:48:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ console.log(timer.A);
 var sysA = require('./lib/procsys');
 sysA().Init(timer.A, 150);
 var sysB = require('./lib/procsys');
-sysB().Init(timer.B, 180);
+sysB().Init(timer.B, 150);
