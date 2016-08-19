d58f1028774a9ca75978a6a1abac0d911fb8449c
vhgrower <vhgrower@gmail.com>
2016-08-19 16:27:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ gpioInit();
 console.log(timer.A);
 
 var sysA = require('./lib/procsys');
-sysA().Init(timer.A, 75);
+sysA().Init(timer.A, 80);
 var sysB = require('./lib/procsys');
 sysB().Init(timer.B, 90);
