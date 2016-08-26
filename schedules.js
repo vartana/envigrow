404094093582075d6d312706bce60211673a887c
vhgrower <vhgrower@gmail.com>
2016-08-26 16:54:36 -0700

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -1,7 +1,7 @@
 // 5 = systems 1-2
-// 13 = systems 3
-// 3 = systems 4-6
-// 12 = system 6-7 box 4 changed on 08-24
+// 13 = systems 3-4
+// 3 = systems 5-6
+// 12 = system 7 -----changed on 08-24
 // 11 = systems 8-9
 // 7 = systems 7-8 -- correction 10-11 box 6 changed on 08-24
 // 8 = systems 12-13
@@ -10,8 +10,7 @@ var SystemA = [
   {circ: 5, feed: 21, sec: 5},
   {circ: 13, feed: 18, sec: 5},
   {circ: 3, feed: 24, sec: 5},
-  {circ: 11, feed: 19, sec: 5},
-  {circ: 12, feed: 22, sec: 5}
+  {circ: 11, feed: 19, sec: 5}
 ];
 
 
@@ -20,7 +19,8 @@ var SystemB = [
   {circ: 12, feed: 23, sec: 25},//circ was 7 changed to 8 for new system Systems 11-12
   {circ: 12, feed: 15, sec: 25},//Systems 13-14
   {circ: 12, feed: 16, sec: 30},//System 15-16
-  {circ: 12, feed: 10, sec: 20}//tents system
+  {circ: 12, feed: 10, sec: 20},
+  {circ: 12, feed: 22, sec: 10}//---this is system 7 in roomb A modified 08 26
 ];
 
 module.exports = { A: SystemA,
