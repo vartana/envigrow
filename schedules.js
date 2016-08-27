b03638019f2be00572d9cdcc1a44cdaa6b4bdfac
vhgrower <vhgrower@gmail.com>
2016-08-26 17:00:03 -0700

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -7,11 +7,11 @@
 // 8 = systems 12-13
 //10 = systems 14-15
 var SystemA = [
-  {circ: 5, feed: 21, sec: 2},
-  {circ: 13, feed: 18, sec: 2},
-  {circ: 3, feed: 24, sec: 2},
-  {circ: 11, feed: 19, sec: 2},
-  {circ: 12, feed: 22, sec: 3}
+  {circ: 5, feed: 21, sec: 5},
+  {circ: 13, feed: 18, sec: 5},
+  {circ: 3, feed: 24, sec: 5},
+  {circ: 11, feed: 19, sec: 5},
+  {circ: 12, feed: 22, sec: 10}
 ];
 
 
