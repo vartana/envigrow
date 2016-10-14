935465ebfe197cf87e23955b3b3315a9933c305b
vhgrower <vhgrower@gmail.com>
2016-10-14 09:48:13 -0700

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -10,17 +10,17 @@ var SystemA = [
   {circ: 5, feed: 21, sec: 12},
   {circ: 13, feed: 18, sec: 12},
   {circ: 3, feed: 24, sec: 12},
-  {circ: 11, feed: 19, sec: 12},
-  {circ: 12, feed: 22, sec: 12}
+  {circ: 11, feed: 19, sec: 12}
 ];
 
 
 var SystemB = [
-  {circ: 12, feed: 7, sec: 5},//added for new system in B System 10
-  {circ: 12, feed: 23, sec: 5},//circ was 7 changed to 8 for new system Systems 11-12
-  {circ: 12, feed: 15, sec: 5},//Systems 13-14
-  {circ: 12, feed: 16, sec: 5},//System 15-16
-  {circ: 12, feed: 10, sec: 5}
+  {circ: 12, feed: 7, sec: 12},//added for new system in B System 10
+  {circ: 12, feed: 23, sec: 12},//circ was 7 changed to 8 for new system Systems 11-12
+  {circ: 12, feed: 15, sec: 12},//Systems 13-14
+  {circ: 12, feed: 16, sec: 12},//System 15-16
+  {circ: 12, feed: 10, sec: 12},
+  {circ: 12, feed: 22, sec: 12} //This is the real System in room A
 ];
 
 module.exports = { A: SystemA,
